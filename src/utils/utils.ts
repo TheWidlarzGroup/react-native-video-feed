1030fe3b856dbda269bcf81a070101b40636dd04
jakubszymanski-twg <jakub.szymanski@thewidlarzgroup.com>
2026-02-02 16:06:09 +0100

    supabase instead of netlify

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,6 @@
-const NETLIFY_BASE = "https://splendorous-muffin-0ddebe.netlify.app";
+const HLS_BASE =
+    "https://pxcgbjvkyhrwimyeqwhn.supabase.co/storage/v1/object/public/hls";
 
 export const SOURCES = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(
-    (i) => `${NETLIFY_BASE}/v${i}/index.m3u8`,
+    (i) => `${HLS_BASE}/v${i}/index.m3u8`,
 );
