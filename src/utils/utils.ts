523b921a3d714bef69e1258fea6910062885e13b
jakubszymanski-twg <jakub.szymanski@thewidlarzgroup.com>
2026-04-21 10:58:27 +0200

    change sources to netlify, podfile fix for latest xcode version

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,5 +1,5 @@
 const HLS_BASE =
-    "https://pxcgbjvkyhrwimyeqwhn.supabase.co/storage/v1/object/public/hls";
+    "https://69e664383f19480597c5a256--twg-video-feed-demo.netlify.app";
 
 export const SOURCES = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map(
     (i) => `${HLS_BASE}/v${i}/index.m3u8`,
